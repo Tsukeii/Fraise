ae90305665d9ae97dca7443ba2a25d4ca1db2971
Nadia <80582495+Tsukeii@users.noreply.github.com>
2022-07-14 11:48:16 +0300

    Update help.js

--- src/commands/general/help.js
+++ src/commands/general/help.js
@@ -1,8 +1,3 @@
-/**
- * Discord Welcome-Bot
- * Copyright (c) 2021 The Welcome-Bot Team and Contributors
- * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
- */
 const beautifyPerms = require("../../functions/beautifyPerms");
 const { Embed, Command } = require("../../classes");
 const { Pagination } = require("djs-pagination-buttons");
