827052d102b0f6e52cf32ab25aed1c859f3f1974
Nadia <80582495+Tsukeii@users.noreply.github.com>
2022-07-14 11:46:01 +0300

    Update shard.js

--- src/shard.js
+++ src/shard.js
@@ -1,14 +1,8 @@
-/**
- * Discord Welcome-Bot
- * Copyright (c) 2021 The Welcome-Bot Team and Contributors
- * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
- */
-
 require("dotenv").config();
 const { ShardingManager } = require("discord.js");
 const manager = new ShardingManager(__dirname + "/bot.js", {
     token: process.env.DISCORD_TOKEN,
 });
 
-manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
+manager.on("shardCreate", (shard) => console.log(`Éclat lancé ${shard.id}`));
 manager.spawn({ timeout: -1 });
